beff83e87485c8736d84a3db0736c14c01ccabeb
YuggY <YuggY@users.noreply.github.com>
2017-08-03 18:32:22 +0100

    T430 Laptop

--- blocks/hardware/laptop/lenovo_thinkpad_T430.sh
+++ blocks/hardware/laptop/lenovo_thinkpad_T430.sh
@@ -21,7 +21,7 @@ echo "options thinkpad_acpi fan_control=1" > /etc/modprobe.d/thinkfan.conf
 # enable blinking wifi led
 cat > /etc/modprobe.d/iwlwifi.conf << EOF
 # led_mode=0 - led_mode:0=system default, 1=On(RF On)/Off(RF Off), 2=blinking, 3=Off (default: 0) (int)
-options iwlwifi led_mode=2
+options iwlwifi led_mode=1
 options iwlwifi power_save=1
 EOF
 
